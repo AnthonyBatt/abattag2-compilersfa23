8ce70ab1906b1b1a853f97f4135122787ba98d2a
Anthony Battaglia <abattag2@nd.edu>
2023-11-09 17:38:50 -0500

    wrote the scope functions, now i have to test

--- scope.h
+++ scope.h
@@ -1,12 +1,17 @@
 #include "symbol.h"
+#include "hash_table.h"
 
-// push hashtable onto stack (function call)
-void scpe_enter(); 
-// pop hashtable from stack (return statement or end of function)
+struct hashtable *ht_global;
+int CURRENT_SCOPE = 0;
+
+
+// push hashtable onto stack (function call) increase depth
+void scope_enter(); 
+// pop hashtable from stack (return statement or end of function) decrease depth
 void scope_exit();
-// returns depth of stack (let's us know what scope we are in)
+// returns depth of stack (let's us know what scope we are in) return depth
 int scope_level();
-// adds (name, pair) to to current scope level
+// adds (name, pair) to current scope level 
 void scope_bind(char *name, struct symbol *s);
 // searches hashtables for exact match to name, if not found return null
 struct symbol *scope_lookup(char *name);
